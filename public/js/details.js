ef71977e1dc2abc58ce089fabe757a4e86ec5fdd
Bhaskar Tripathi <bhaskar.tripathi@outlook.com>
2020-03-07 02:16:28 -0800

    changes

--- public/js/details.js
+++ public/js/details.js
@@ -1,15 +1,50 @@
+/* eslint-disable semi */
 /* eslint-disable no-undef */
 $(document).ready(function () {
+  google.books.load();
+
   $('#searchBtn').on('click', function () {
     var searchText = $('#searchText').val()
-    $.ajax('/api/search/:' + searchText, {
+    $.ajax('/api/search/' + searchText, {
       type: 'GET'
-      // data: devouredState
     }).then(function (response) {
       document.open()
       document.write(response)
       document.close()
       console.log('loaded')
     })
-  })
+  });
+  $('.open-button').on('click', function () {
+    var volumeid = $(this).data('id');
+    $.ajax('/details/' + volumeid, {
+      type: 'GET'
+    }).then(function (response) {
+      document.open()
+      document.write(response);
+      document.close()
+      console.log('loaded')
+    })
+  });
+  $('.add-button').on('click', function () {
+    var volumeid = $(this).data('id');
+    var volinfo = $(this).data('volumeinfo');
+    console.log(volinfo);
+    $.ajax('/api/book/' + volumeid, {
+      type: 'POST',
+      data: {
+        title: $(this).data('title'),
+        author: $(this).data('author'),
+        isbn: $(this).data('isbn')
+      }
+    }).then(function (response) {
+      console.log(response);
+    })
+  });
+
+  $('#viewBook-button').on('click', function () {
+    var isbn = $(this).data('isbn');
+    var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
+      viewer.load(`ISBN:${isbn}`);
+      $('#bookViewerModal').modal('show');
+  });
 })
